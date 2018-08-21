941a71b9567c9fdb25e9e039dade330847f61e98
Alex Pletzer <alexander@gokliya.net>
2018-08-21 14:34:11 +1200

    fixed 4*pi bug

--- scatter.py
+++ scatter.py
@@ -5,9 +5,9 @@ from matplotlib import pyplot
 from numpy import cos, sin, pi
 
 parser = argparse.ArgumentParser(description='Compute field scattered by an obstacle.')
-parser.add_argument('-n', dest='n', type=int, default=10, help='number of segments')
-parser.add_argument('-cx', dest='cx', type=str, default='cos(2*pi*t)', help='x contour expression of 0 <= t <= 1')
-parser.add_argument('-cy', dest='cy', type=str, default='sin(2*pi*t)', help='y contour expression of 0 <= t <= 1')
+parser.add_argument('-n', dest='n', type=int, default=100, help='number of segments')
+parser.add_argument('-xc', dest='xContourExpr', type=str, default='cos(2*pi*t)', help='x contour expression of 0 <= t <= 1')
+parser.add_argument('-yc', dest='yContourExpr', type=str, default='sin(2*pi*t)', help='y contour expression of 0 <= t <= 1')
 parser.add_argument('-lambda', dest='lmbda', type=float, default=0.5, help='x wavenumber')
 parser.add_argument('-plot', dest='plot', action='store_false', help='plot solution')
 
@@ -15,6 +15,7 @@ args = parser.parse_args()
 
 # incident wavenumber
 knum = 2 * numpy.pi / args.lmbda
+kvec = numpy.array([knum, 0.], numpy.float64)
 ikvec = numpy.array([knum*1j, 0.], numpy.complex64)
 
 def incidentPsi(p):
@@ -25,23 +26,37 @@ def incidentNormalGradPsi(p, nvec):
 
 def normalVector(p0, p1):
 	dx, dy = p1 - p0
-	return numpy.array([dy, -dx], numpy.complex64) / numpy.sqrt(dx*dx + dy*dy)
+	return numpy.array([dy, -dx], numpy.float64) / numpy.sqrt(dx*dx + dy*dy)
+
+def greenFunction(pa, pb):
+	# distance cannot be zero
+	dp = pb - pa
+	r = max(numpy.sqrt(numpy.dot(dp, dp)), 1.e-8)
+	return 1j*numpy.pi * scipy.special.hankel1(0, knum * r)
+
+def normalGradientGreenFunction(nvec, pa, pb):
+	# distance cannot be zero
+	dp = pb - pa
+	r = max(numpy.sqrt(numpy.dot(dp, dp)), 1.e-8)
+	gradR = dp / r
+	return -1j*numpy.pi * scipy.special.hankel1(1, knum * r) * knum * nvec.dot(gradR)
 
 # create grid
 nx, ny = 100, 100
-xmin, xmax = -10*args.lmbda, 10*args.lmbda
-ymin, ymax = -10*args.lmbda, 10*args.lmbda
+xmin, xmax = -20*args.lmbda, 10*args.lmbda
+ymin, ymax = -10*args.lmbda, 20*args.lmbda
 xg = numpy.linspace(xmin, xmax, nx + 1)
 yg = numpy.linspace(ymin, ymax, ny + 1)
 
 # contour points of the obstacle
 t = numpy.linspace(0., 1., args.n + 1)
-xc = eval(args.cx)
-yc = eval(args.cy)
+xc = eval(args.xContourExpr)
+yc = eval(args.yContourExpr)
 
 # compute the field
 psi_scattered = numpy.zeros((ny + 1, nx + 1), numpy.complex64)
 psi_incident = numpy.zeros((ny + 1, nx + 1), numpy.complex64)
+fourPi = 4. * numpy.pi
 for j in range(ny + 1):
 	y = yg[j]
 	for i in range(nx + 1):
@@ -49,6 +64,10 @@ for j in range(ny + 1):
 		# need to check that x,y are outside contour
 		# otherwise continue
 		p = numpy.array([x, y])
+
+		# value of the incident wave on the observation point
+		psi_incident[j, i] = incidentPsi(p)
+
 		for el0 in range(args.n):
 			el1 = el0 + 1
 
@@ -59,34 +78,34 @@ for j in range(ny + 1):
 			# mid point of the segment
 			pm = 0.5*(p0 + p1)
 
-			# vector from mid segment to observation point
-			rho = p - pm
-
-			# distance from mid-point to observation point, cannot be zero
-			r = max(numpy.sqrt(rho.dot(rho)),  1.e-6)
-
 			# normal vector, p0 -> p1 in anticlockwise direction 
 			nvec = normalVector(p0, p1)
 
-			# segment length
-			ds = numpy.sqrt(numpy.dot(p1 - p0, p1 - p0))
+			# vector from mid segment to observation point
+			rho = p - pm
 
-			# value of the incident wave on the observation point
-			psi_incident[j, i] = incidentPsi(p)
+			# segment length
+			delta = p1 - p0
+			ds = numpy.sqrt(delta.dot(delta))
 
 			# normal gradient of the incident wave on the segment mid point
 			normalGradIncidentPsiOnObstacle = incidentNormalGradPsi(pm, nvec)
 
-			# Green function
-			green = 1j * numpy.pi * scipy.special.hankel1(0, knum * r) 
-
-			psi_scattered[j, i] += ds * normalGradIncidentPsiOnObstacle * green / (- 4.*numpy.pi)
+			psi_scattered[j, i] += (ds/fourPi) * greenFunction(pm, p) * normalGradIncidentPsiOnObstacle
+			if nvec.dot(kvec) > 0:
+				# shadow
+				# psi is approximately zero
+				# psi_scattered ~ - psi_incident				
+				psi_scattered[j, i] -= (ds/fourPi) * normalGradientGreenFunction(nvec, pm, p) * incidentPsi(pm)
 
 
 if args.plot:
 	# plot
 	#pyplot.contour(xg, yg, psi_scattered.real, levels=numpy.linspace(-2., 2., 11), linestyles='dashed')
-	pyplot.contour(xg, yg, numpy.real(psi_incident + psi_scattered), levels=numpy.linspace(-2., 2., 21))
+	#p = pyplot.contour(xg, yg, numpy.real(psi_incident + psi_scattered), levels=numpy.linspace(-2., 2., 11))
+	p = pyplot.pcolor(xg, yg, numpy.real(psi_scattered + psi_incident), cmap='coolwarm', vmin=-2., vmax=2.)
+	#p = pyplot.pcolor(xg, yg, numpy.real(psi_incident), cmap='coolwarm')
+	pyplot.colorbar(p)
 
 	# add the obstacle contour
 	pyplot.plot(xc, yc, 'k-')
