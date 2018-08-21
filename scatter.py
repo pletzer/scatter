b5699a547d07d17beef05c8aae7c8b660d70b116
Alex Pletzer <alexander@gokliya.net>
2018-08-21 16:00:06 +1200

    work in progress

--- scatter.py
+++ scatter.py
@@ -3,6 +3,8 @@ import numpy
 import scipy.special
 from matplotlib import pyplot
 from numpy import cos, sin, pi
+import math
+import matplotlib.animation as animation
 
 parser = argparse.ArgumentParser(description='Compute field scattered by an obstacle.')
 parser.add_argument('-n', dest='n', type=int, default=100, help='number of segments')
@@ -17,6 +19,9 @@ args = parser.parse_args()
 knum = 2 * numpy.pi / args.lmbda
 kvec = numpy.array([knum, 0.], numpy.float64)
 ikvec = numpy.array([knum*1j, 0.], numpy.complex64)
+
+fourPi = 4. * numpy.pi
+twoPi = 2. * numpy.pi
 
 def incidentPsi(p):
 	return numpy.exp(ikvec.dot(p))
@@ -41,22 +46,32 @@ def normalGradientGreenFunction(nvec, pa, pb):
 	gradR = dp / r
 	return -1j*numpy.pi * scipy.special.hankel1(1, knum * r) * knum * nvec.dot(gradR)
 
-# create grid
-nx, ny = 100, 100
-xmin, xmax = -20*args.lmbda, 10*args.lmbda
-ymin, ymax = -10*args.lmbda, 20*args.lmbda
-xg = numpy.linspace(xmin, xmax, nx + 1)
-yg = numpy.linspace(ymin, ymax, ny + 1)
+def isInsideContour(p, xc, yc):
+	tot = 0.0
+	for i0 in range(len(xc) - 1):
+		i1 = i0 + 1
+		a = numpy.array([xc[i0], yc[i0]]) - p
+		b = numpy.array([xc[i1], yc[i1]]) - p
+		tot += math.atan2(a[0]*b[1] - a[1]*b[0], a.dot(b))
+	tot /= twoPi
+	return (abs(tot) > 0.1)
 
 # contour points of the obstacle
 t = numpy.linspace(0., 1., args.n + 1)
 xc = eval(args.xContourExpr)
 yc = eval(args.yContourExpr)
 
+# create grid
+nx, ny = 100, 100
+xmin, xmax = xc.min() - 5*args.lmbda, xc.max() + 3*args.lmbda
+ymin, ymax = yc.min() - 3*args.lmbda, yc.max() + 4*args.lmbda
+xg = numpy.linspace(xmin, xmax, nx + 1)
+yg = numpy.linspace(ymin, ymax, ny + 1)
+
+
 # compute the field
 psi_scattered = numpy.zeros((ny + 1, nx + 1), numpy.complex64)
 psi_incident = numpy.zeros((ny + 1, nx + 1), numpy.complex64)
-fourPi = 4. * numpy.pi
 for j in range(ny + 1):
 	y = yg[j]
 	for i in range(nx + 1):
@@ -64,6 +79,9 @@ for j in range(ny + 1):
 		# need to check that x,y are outside contour
 		# otherwise continue
 		p = numpy.array([x, y])
+
+		if isInsideContour(p, xc, yc):
+			continue
 
 		# value of the incident wave on the observation point
 		psi_incident[j, i] = incidentPsi(p)
@@ -92,22 +110,37 @@ for j in range(ny + 1):
 			normalGradIncidentPsiOnObstacle = incidentNormalGradPsi(pm, nvec)
 
 			psi_scattered[j, i] += (ds/fourPi) * greenFunction(pm, p) * normalGradIncidentPsiOnObstacle
+			
 			if nvec.dot(kvec) > 0:
 				# shadow
 				# psi is approximately zero
 				# psi_scattered ~ - psi_incident				
 				psi_scattered[j, i] -= (ds/fourPi) * normalGradientGreenFunction(nvec, pm, p) * incidentPsi(pm)
-
+			
 
 if args.plot:
+
+	nanim = 20
+	dOmegaTime = twoPi / float(nanim)
+
+	fig, ax = pyplot.subplots()
+	zdata = numpy.real(numpy.exp(1j*0*dOmegaTime) * (psi_scattered + psi_incident))
+	p = pyplot.pcolormesh(xg, yg, zdata, cmap='coolwarm', vmin=-2., vmax=2.)
+
+	pyplot.plot(xc, yc, 'k-')
+
+	def animate(i):
+		z = numpy.real(numpy.exp(1j*i*dOmegaTime) * (psi_scattered + psi_incident))
+		p.set_array(z.ravel())
 	# plot
 	#pyplot.contour(xg, yg, psi_scattered.real, levels=numpy.linspace(-2., 2., 11), linestyles='dashed')
 	#p = pyplot.contour(xg, yg, numpy.real(psi_incident + psi_scattered), levels=numpy.linspace(-2., 2., 11))
-	p = pyplot.pcolor(xg, yg, numpy.real(psi_scattered + psi_incident), cmap='coolwarm', vmin=-2., vmax=2.)
 	#p = pyplot.pcolor(xg, yg, numpy.real(psi_incident), cmap='coolwarm')
 	pyplot.colorbar(p)
 
 	# add the obstacle contour
-	pyplot.plot(xc, yc, 'k-')
-	pyplot.axes().set_aspect('equal')
+	
+	#pyplot.axes().set_aspect('equal')
+	ani = animation.FuncAnimation(fig, animate, numpy.arange(1, nanim + 1), 
+                              interval=25,)
 	pyplot.show()
