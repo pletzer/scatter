c9641ef2dc3cbc377b4f5018e725b9f01a18947a
Alex Pletzer <alexander@gokliya.net>
2018-09-09 10:06:23 +1200

    renaming ns -> nc

--- scatter.py
+++ scatter.py
@@ -10,7 +10,7 @@ parser = argparse.ArgumentParser(description='Compute field scattered by an obst
 parser.add_argument('-lambda', dest='lmbda', type=float, default=0.5, help='x wavelength')
 parser.add_argument('-nx', dest='nx', type=int, default=100, help='number of x cells')
 parser.add_argument('-ny', dest='ny', type=int, default=100, help='number of y cells')
-parser.add_argument('-nc', dest='ns', type=int, default=100, help='number of contour segments')
+parser.add_argument('-nc', dest='nc', type=int, default=100, help='number of contour segments')
 parser.add_argument('-xc', dest='xContourExpr', type=str, default='cos(2*pi*t + 0.5*sin(2*pi*t + 0.9))', help='x contour expression of 0 <= t <= 1')
 parser.add_argument('-yc', dest='yContourExpr', type=str, default='sin(2*pi*t)', help='y contour expression of 0 <= t <= 1')
 parser.add_argument('-save', dest='save', action='store_false', help='do not save time varying solution in VTK files')
@@ -48,7 +48,7 @@ def isInsideContour(p, xc, yc, tol=0.01):
 	return (abs(tot) > tol)
 
 # contour points of the obstacle
-t = numpy.linspace(0., 1., args.ns + 1)
+t = numpy.linspace(0., 1., args.nc + 1)
 xc = eval(args.xContourExpr)
 yc = eval(args.yContourExpr)
 
