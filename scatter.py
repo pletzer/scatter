06e536f10ec9e4f4f8d7bc6088f7d5ab85ec64b7
Alex Pletzer <alexander@gokliya.net>
2018-09-09 10:05:08 +1200

    + commenting isInside

--- scatter.py
+++ scatter.py
@@ -7,12 +7,12 @@ import saveVtk
 import wave
 
 parser = argparse.ArgumentParser(description='Compute field scattered by an obstacle.')
-parser.add_argument('-ns', dest='ns', type=int, default=100, help='number of segments')
+parser.add_argument('-lambda', dest='lmbda', type=float, default=0.5, help='x wavelength')
 parser.add_argument('-nx', dest='nx', type=int, default=100, help='number of x cells')
 parser.add_argument('-ny', dest='ny', type=int, default=100, help='number of y cells')
+parser.add_argument('-nc', dest='ns', type=int, default=100, help='number of contour segments')
 parser.add_argument('-xc', dest='xContourExpr', type=str, default='cos(2*pi*t + 0.5*sin(2*pi*t + 0.9))', help='x contour expression of 0 <= t <= 1')
 parser.add_argument('-yc', dest='yContourExpr', type=str, default='sin(2*pi*t)', help='y contour expression of 0 <= t <= 1')
-parser.add_argument('-lambda', dest='lmbda', type=float, default=0.5, help='x wavelength')
 parser.add_argument('-save', dest='save', action='store_false', help='do not save time varying solution in VTK files')
 parser.add_argument('-checksum', dest='checksum', action='store_true', help='compute and print a checksum of the scattered wave')
 
@@ -25,7 +25,19 @@ twoPi = 2. * numpy.pi
 knum = 2 * numpy.pi / args.lmbda
 kvec = numpy.array([knum, 0.,], numpy.float64)
 
-def isInsideContour(p, xc, yc):
+def isInsideContour(p, xc, yc, tol=0.01):
+	"""
+	Check is a point is inside closed contour by summing the 
+	the angles between point p, (xc[i], yc[i]) and (xc[i+1], yc[i+1]).
+	Point p id declared ot be inside if the total angle amounts to 
+	2*pi.
+
+	@param p point (2d array)
+	@param xc array of x points, anticlockwise and must close
+	@param yc array of y points, anticlockwise and must close
+	@param tol tolerance
+	@return True if p is inside, False otherwise
+	"""
 	tot = 0.0
 	for i0 in range(len(xc) - 1):
 		i1 = i0 + 1
@@ -33,7 +45,7 @@ def isInsideContour(p, xc, yc):
 		b = numpy.array([xc[i1], yc[i1]]) - p[:2]
 		tot += math.atan2(a[0]*b[1] - a[1]*b[0], a.dot(b))
 	tot /= twoPi
-	return (abs(tot) > 0.1)
+	return (abs(tot) > tol)
 
 # contour points of the obstacle
 t = numpy.linspace(0., 1., args.ns + 1)
@@ -60,6 +72,7 @@ for j in range(ny + 1):
 		# otherwise continue
 		p = numpy.array([x, y,])
 
+		# skip if poiont is inside closed contour
 		if isInsideContour(p, xc, yc):
 			continue
 
