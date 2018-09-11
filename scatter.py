2e03eab5e9bf2ff60ee27ed2e92aa818d2803986
Alex Pletzer <alexander@gokliya.net>
2018-09-11 01:30:09 +0000

    t -> '    ' (tabs to spaces)

--- scatter.py
+++ scatter.py
@@ -26,26 +26,26 @@ knum = 2 * numpy.pi / args.lmbda
 kvec = numpy.array([knum, 0.,], numpy.float64)
 
 def isInsideContour(p, xc, yc, tol=0.01):
-	"""
-	Check is a point is inside closed contour by summing the 
-	the angles between point p, (xc[i], yc[i]) and (xc[i+1], yc[i+1]).
-	Point p id declared to be inside if the total angle amounts to 
-	2*pi.
+    """
+    Check is a point is inside closed contour by summing the 
+    the angles between point p, (xc[i], yc[i]) and (xc[i+1], yc[i+1]).
+    Point p id declared to be inside if the total angle amounts to 
+    2*pi.
 
-	@param p point (2d array)
-	@param xc array of x points, anticlockwise and must close
-	@param yc array of y points, anticlockwise and must close
-	@param tol tolerance
-	@return True if p is inside, False otherwise
-	"""
-	tot = 0.0
-	for i0 in range(len(xc) - 1):
-		i1 = i0 + 1
-		a = numpy.array([xc[i0], yc[i0]]) - p[:2]
-		b = numpy.array([xc[i1], yc[i1]]) - p[:2]
-		tot += math.atan2(a[0]*b[1] - a[1]*b[0], a.dot(b))
-	tot /= twoPi
-	return (abs(tot) > tol)
+    @param p point (2d array)
+    @param xc array of x points, anticlockwise and must close
+    @param yc array of y points, anticlockwise and must close
+    @param tol tolerance
+    @return True if p is inside, False otherwise
+    """
+    tot = 0.0
+    for i0 in range(len(xc) - 1):
+        i1 = i0 + 1
+        a = numpy.array([xc[i0], yc[i0]]) - p[:2]
+        b = numpy.array([xc[i1], yc[i1]]) - p[:2]
+        tot += math.atan2(a[0]*b[1] - a[1]*b[0], a.dot(b))
+    tot /= twoPi
+    return (abs(tot) > tol)
 
 # contour points of the obstacle
 t = numpy.linspace(0., 1., args.nc + 1)
@@ -64,28 +64,28 @@ yg = numpy.linspace(ymin, ymax, ny + 1)
 scat = numpy.zeros((ny + 1, nx + 1), numpy.complex64)
 inci = numpy.zeros((ny + 1, nx + 1), numpy.complex64)
 for j in range(ny + 1):
-	y = yg[j]
-	for i in range(nx + 1):
-		x = xg[i]
+    y = yg[j]
+    for i in range(nx + 1):
+        x = xg[i]
 
-		# need to check that x,y are outside contour
-		# otherwise continue
-		p = numpy.array([x, y,])
+        # need to check that x,y are outside contour
+        # otherwise continue
+        p = numpy.array([x, y,])
 
-		# skip if poiont is inside closed contour
-		if isInsideContour(p, xc, yc):
-			continue
+        # skip if poiont is inside closed contour
+        if isInsideContour(p, xc, yc):
+            continue
 
-		inci[j, i] = wave.incident(kvec, p)
-		scat[j, i] = wave.computeScatteredWave(kvec, xc, yc, p)
+        inci[j, i] = wave.incident(kvec, p)
+        scat[j, i] = wave.computeScatteredWave(kvec, xc, yc, p)
 
 if args.checksum:
-	print('Squared sum of scattered field amplitudes: {}'.format((scat*numpy.conj(scat)).sum().real))
+    print('Squared sum of scattered field amplitudes: {}'.format((scat*numpy.conj(scat)).sum().real))
 
 if args.save:
-	# number of time frames
-	nanim = 20
-	dOmegaTime = twoPi / float(nanim)
-	for it in range(nanim):
-		data = numpy.real(numpy.exp(-1j*it*dOmegaTime) * (scat + inci))
-		saveVtk.saveData('scatter_{}.vtk'.format(it), xg, yg, data, 'total')
+    # number of time frames
+    nanim = 20
+    dOmegaTime = twoPi / float(nanim)
+    for it in range(nanim):
+        data = numpy.real(numpy.exp(-1j*it*dOmegaTime) * (scat + inci))
+        saveVtk.saveData('scatter_{}.vtk'.format(it), xg, yg, data, 'total')
--- wave.py
+++ wave.py
@@ -7,95 +7,95 @@ FOURPI = 2. * TWOPI
 
 
 def incident(kvec, point):
-	"""
-	Incident wave
+    """
+    Incident wave
 
-	@param kvec incident wave vector
-	@param point target point
-	@return complex number
-	"""
-	return numpy.exp(1j*kvec.dot(point))
+    @param kvec incident wave vector
+    @param point target point
+    @return complex number
+    """
+    return numpy.exp(1j*kvec.dot(point))
 
 
 def gradIncident(nvec, kvec, point):
-	"""
-	Normal gradient of the incident wave, assumes incident wave is exp(1j * kvec.x)
+    """
+    Normal gradient of the incident wave, assumes incident wave is exp(1j * kvec.x)
 
-	@param nvec normal vector pointing inwards
-	@param kvec incident wave vector
-	@param point (source) point
-	@return complex number
-	"""
-	return 1j*nvec.dot(kvec)*incident(kvec, point)
+    @param nvec normal vector pointing inwards
+    @param kvec incident wave vector
+    @param point (source) point
+    @return complex number
+    """
+    return 1j*nvec.dot(kvec)*incident(kvec, point)
 
 
 def computeScatteredWaveElement(kvec, p0, p1, point):
-	"""
-	Scattered wave contribution from a single segment
-	@param kvec incident wave vector
-	@param p0 starting point of the segment
-	@param p1 end point of the segment
-	@param point observer point
-	@return complex value
-	"""
+    """
+    Scattered wave contribution from a single segment
+    @param kvec incident wave vector
+    @param p0 starting point of the segment
+    @param p1 end point of the segment
+    @param point observer point
+    @return complex value
+    """
 
-	# xdot is anticlockwise
-	xdot = p1 - p0
+    # xdot is anticlockwise
+    xdot = p1 - p0
 
-	# mid point of the segment
-	pmid = 0.5*(p0 + p1)
+    # mid point of the segment
+    pmid = 0.5*(p0 + p1)
 
-	# segment length
-	dsdt = numpy.sqrt(xdot.dot(xdot))
+    # segment length
+    dsdt = numpy.sqrt(xdot.dot(xdot))
 
-	# normal vector, pointintg inwards and normalised
-	nvec = numpy.array([-xdot[1], xdot[0],])
-	nvec /= numpy.sqrt(nvec.dot(nvec))
+    # normal vector, pointintg inwards and normalised
+    nvec = numpy.array([-xdot[1], xdot[0],])
+    nvec /= numpy.sqrt(nvec.dot(nvec))
 
-	# from segment mid-point to observer
-	rvec = point - pmid
-	r = numpy.sqrt(rvec.dot(rvec))
+    # from segment mid-point to observer
+    rvec = point - pmid
+    r = numpy.sqrt(rvec.dot(rvec))
 
-	kmod = numpy.sqrt(kvec.dot(kvec))
-	kr = kmod * r
+    kmod = numpy.sqrt(kvec.dot(kvec))
+    kr = kmod * r
 
-	# Green functions and normal derivatives
-	g = (1j/4.) * hankel1(0, kr)
-	dgdn = (-1j/4.) * hankel1(1, kr) * kmod * nvec.dot(rvec) / r
+    # Green functions and normal derivatives
+    g = (1j/4.) * hankel1(0, kr)
+    dgdn = (-1j/4.) * hankel1(1, kr) * kmod * nvec.dot(rvec) / r
 
-	# contribution from the gradient of the incident wave on the surface
-	# of the obstacle. The normal derivative of the scattered wave is 
-	# - normal derivative of the incident wave.
-	scattered_wave = - dsdt * g * gradIncident(nvec, kvec, pmid)
+    # contribution from the gradient of the incident wave on the surface
+    # of the obstacle. The normal derivative of the scattered wave is 
+    # - normal derivative of the incident wave.
+    scattered_wave = - dsdt * g * gradIncident(nvec, kvec, pmid)
 
-	# shadow side: total wave is nearly zero 
-	#              => scattered wave amplitude = -incident wave ampl.
-	#
-	# illuminated side:
-	#              => scattered wave amplitude = +incident wave ampl.
-	shadow = 2*((nvec.dot(kvec) > 0.) - 0.5) # +1 on the shadow side, -1 on the illuminated side
-	scattered_wave += shadow * dsdt * dgdn * incident(kvec, pmid)
+    # shadow side: total wave is nearly zero 
+    #              => scattered wave amplitude = -incident wave ampl.
+    #
+    # illuminated side:
+    #              => scattered wave amplitude = +incident wave ampl.
+    shadow = 2*((nvec.dot(kvec) > 0.) - 0.5) # +1 on the shadow side, -1 on the illuminated side
+    scattered_wave += shadow * dsdt * dgdn * incident(kvec, pmid)
 
-	return scattered_wave
+    return scattered_wave
 
 
 def computeScatteredWave(kvec, xc, yc, point):
-	"""
-	Total scattered wave response, summing up 
-	contributions from each segment
+    """
+    Total scattered wave response, summing up 
+    contributions from each segment
 
-	@param kvec incident wave vector
-	@param xc list of x coordinates representing the contour, must close
-	@param yc list of y coordinates representing the contour, must close
-	@param point observer point
-	@return complex value
-	"""
-	res = 0j
-	n = len(xc)
-	for i0 in range(n - 1):
-		p0 = numpy.array([xc[i0], yc[i0],])
-		i1 = i0 + 1
-		p1 = numpy.array([xc[i1], yc[i1],])
-		res += computeScatteredWaveElement(kvec, p0, p1, point)
-	return res
+    @param kvec incident wave vector
+    @param xc list of x coordinates representing the contour, must close
+    @param yc list of y coordinates representing the contour, must close
+    @param point observer point
+    @return complex value
+    """
+    res = 0j
+    n = len(xc)
+    for i0 in range(n - 1):
+        p0 = numpy.array([xc[i0], yc[i0],])
+        i1 = i0 + 1
+        p1 = numpy.array([xc[i1], yc[i1],])
+        res += computeScatteredWaveElement(kvec, p0, p1, point)
+    return res
 
