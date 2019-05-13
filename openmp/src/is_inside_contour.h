3755bd12499b08fbc7e894b10c5c9efafabe6380
Alex Pletzer <alexander@gokliya.net>
2019-05-13 21:33:39 +1200

    need to test

--- openmp/src/is_inside_contour.h
+++ openmp/src/is_inside_contour.h
@@ -4,22 +4,16 @@
 #ifndef IS_INSIDE_CONTOUR_H
 #define IS_INSIDE_CONTOUR_H
 
-#define TWOPI 2. * M_PI
-
 /**
- * Check is a point is inside closed contour by summing the 
- * the angles between point p, (xc[i], yc[i]) and (xc[i+1], yc[i+1]).
- * Point p id declared to be inside if the total angle amounts to 
- * 2*pi.
+ * Check is a point is inside closed contour
  *
  * @param p point (2d array)
  * @param n number of points
  * @param xc array of x points, anticlockwise and must close
  * @param yc array of y points, anticlockwise and must close
- * @param tol tolerance
  * @return 1 if p is inside, 0 otherwise
  */
 extern "C" int
-isInsideContour(const double p[], int n, const double xc[], const double yc[], const double tol);
+isInsideContour(const double p[], int n, const double xc[], const double yc[]);
 
 #endif // IS_INSIDE_CONTOUR_H
